72a37fe84e0ceb9c52f3175956ba0370c566ade1
hudaokeji <github@hudaokeji.com>
2019-12-27 20:37:30 +0900

    init commit

---
+++ dist/skylark-jquery-caret/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-jquery-caret - A version of jquery caret that ported to running on skylarkjs
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-jquery-caret/
+ * @license MIT
+ */
+define(["./caret"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ dist/uncompressed/skylark-jquery-caret/main.js
@@ -0,0 +1,3 @@
+define(["./caret"],function($){
+	return $;
+})
